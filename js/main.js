2bd35fbe265eb8677451e0c282ea1871115596f9
Eduardo <70926975+librotero@users.noreply.github.com>
2022-04-18 12:47:57 -0300

    mejoras

--- js/main.js
+++ js/main.js
@@ -15,9 +15,9 @@ window.onscroll = function (){
 }
 
 document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+
 menu =document.getElementById("header");
 nav=document.getElementById("nav");
-
 function mostrar_menu(){
 menu.classList.toggle('move_content');
 nav.classList.toggle('move_nav');
