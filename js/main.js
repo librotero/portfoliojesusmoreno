472655133be43684a6d1d0e93cec406cba61ad36
Eduardo <70926975+librotero@users.noreply.github.com>
2022-04-18 03:54:40 -0300

    elementos de barar

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-//creamos 
-window.onscroll= function(){
-    scroll=document.docuementElement.scrollTop;
+
+window.onscroll = function (){
+
+    scroll = document.documentElement.scrollTop;
 
     header = document.getElementById("header");
 
-    if(scroll>20){
-    header.classList.add('nav_mod')
+    if (scroll > 500){
+        header.classList.add('nav_mod');
+
+    }else if(scroll < 500){
+        header.classList.remove('nav_mod');
     }
+
+}
+
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+menu =document.getElementById("header");
+nav=document.getElementById("nav");
+
+function mostrar_menu(){
+menu.classList.toggle('move_content');
+nav.classList.toggle('move_nav');
+
 }
